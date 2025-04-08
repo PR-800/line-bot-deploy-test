4354c6abc548308534ddc0a0112648e18ae06ee8
PR-800 <titipa.eamsiriwong@gmail.com>
2025-04-08 22:27:50 +0700

    edit index.js

--- functions/index.js
+++ functions/index.js
@@ -1,30 +1,36 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 const port = 3000;
 
 const line = require("./utils/line");
 const gemini = require("./utils/gemini");
 
-app.use(bodyParser.json());
 
 app.post('/webhook', async (req, res) => {
-  const events = req.body.events;
-    for (const event of events) {
-      switch (event.type) {
-        case "message":
-          if (event.message.type === "text") {
-            const msg = await gemini.chat(event.message.text);
-            await line.reply(event.replyToken, [{ type: "text", text: msg }]);
-            break;
-          }
-          if (event.message.type === "image") {
-            const imageBinary = await line.getImageBinary(event.message.id);
-            const msg = await gemini.multimodal(imageBinary);
-            await line.reply(event.replyToken, [{ type: "text", text: msg }]);
-            break;
-          }
-          break;
-      }
-    }
-  res.send(req.method);
+  console.log('Received LINE Event:', req.body);
+  res.sendStatus(200);
+  // const events = req.body.events;
+  //   for (const event of events) {
+  //     switch (event.type) {
+  //       case "message":
+  //         if (event.message.type === "text") {
+  //           const msg = await gemini.chat(event.message.text);
+  //           await line.reply(event.replyToken, [{ type: "text", text: msg }]);
+  //           break;
+  //         }
+  //         if (event.message.type === "image") {
+  //           const imageBinary = await line.getImageBinary(event.message.id);
+  //           const msg = await gemini.multimodal(imageBinary);
+  //           await line.reply(event.replyToken, [{ type: "text", text: msg }]);
+  //           break;
+  //         }
+  //         break;
+  //     }
+  //   }
+  // res.send(req.method);
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
