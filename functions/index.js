5218a7fa5b5be43755fe67427db20c3eb29184f9
PR-800 <titipa.eamsiriwong@gmail.com>
2025-04-08 21:49:21 +0700

    add gemini key

--- functions/index.js
+++ functions/index.js
@@ -1,58 +1,28 @@
-// index.js
-
 const express = require('express');
-const bodyParser = require('body-parser');
-const line = require('./utils/line'); // ยังใช้ได้เหมือนเดิม
-
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-app.use(bodyParser.json());
+const line = require("./utils/line");
+const gemini = require("./utils/gemini");
 
 app.post('/webhook', async (req, res) => {
   const events = req.body.events;
-  for (const event of events) {
-    switch (event.type) {
-      case 'message':
-        if (event.message.type === 'text') {
-          // reply logic
-        }
-        if (event.message.type === 'image') {
-          // reply logic
-        }
-        break;
+    for (const event of events) {
+      switch (event.type) {
+        case "message":
+          if (event.message.type === "text") {
+            const msg = await gemini.chat(event.message.text);
+            await line.reply(event.replyToken, [{ type: "text", text: msg }]);
+            break;
+          }
+          if (event.message.type === "image") {
+            const imageBinary = await line.getImageBinary(event.message.id);
+            const msg = await gemini.multimodal(imageBinary);
+            await line.reply(event.replyToken, [{ type: "text", text: msg }]);
+            break;
+          }
+          break;
+      }
     }
-  }
-  res.send('OK');
+  res.send(req.method);
 });
-
-app.listen(port, () => {
-  console.log(`LINE webhook listening on port ${port}`);
-});
-
-// scp -i ~/Desktop/LineBotTest/line-ws-key.pem -r ./functions ec2-user@18.142.57.162:/home/ec2-user/myproject/
-
-
-// // Import dependencies
-// const {onRequest} = require("firebase-functions/v2/https");
-// const line = require("./utils/line");
-
-// // Create a webhook via HTTP requests
-// exports.webhook = onRequest(async (req, res) => {
-//   if (req.method === "POST") {
-//     const events = req.body.events;
-//     for (const event of events) {
-//       switch (event.type) {
-//         case "message":
-//           if (event.message.type === "text") {
-//           }
-//           if (event.message.type === "image") {
-//           }
-//           break;
-//       }
-//     }
-//   }
-//   res.send(req.method);
-// });
-
-
