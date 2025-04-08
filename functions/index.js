a478f62b3ecf0c93480b89492eacf16590c2352a
PR-800 <titipa.eamsiriwong@gmail.com>
2025-04-08 22:36:36 +0700

    ssl pass >> edit index.js

--- functions/index.js
+++ functions/index.js
@@ -8,27 +8,27 @@ const gemini = require("./utils/gemini");
 
 
 app.post('/webhook', async (req, res) => {
-  console.log('Received LINE Event:', req.body);
-  res.sendStatus(200);
-  // const events = req.body.events;
-  //   for (const event of events) {
-  //     switch (event.type) {
-  //       case "message":
-  //         if (event.message.type === "text") {
-  //           const msg = await gemini.chat(event.message.text);
-  //           await line.reply(event.replyToken, [{ type: "text", text: msg }]);
-  //           break;
-  //         }
-  //         if (event.message.type === "image") {
-  //           const imageBinary = await line.getImageBinary(event.message.id);
-  //           const msg = await gemini.multimodal(imageBinary);
-  //           await line.reply(event.replyToken, [{ type: "text", text: msg }]);
-  //           break;
-  //         }
-  //         break;
-  //     }
-  //   }
-  // res.send(req.method);
+  // console.log('📨 Received LINE event:', JSON.stringify(req.body, null, 2));
+  // res.sendStatus(200);
+  const events = req.body.events;
+    for (const event of events) {
+      switch (event.type) {
+        case "message":
+          if (event.message.type === "text") {
+            const msg = await gemini.chat(event.message.text);
+            await line.reply(event.replyToken, [{ type: "text", text: msg }]);
+            break;
+          }
+          if (event.message.type === "image") {
+            const imageBinary = await line.getImageBinary(event.message.id);
+            const msg = await gemini.multimodal(imageBinary);
+            await line.reply(event.replyToken, [{ type: "text", text: msg }]);
+            break;
+          }
+          break;
+      }
+    }
+  res.send(req.method);
 });
 
 app.listen(3000, () => {
